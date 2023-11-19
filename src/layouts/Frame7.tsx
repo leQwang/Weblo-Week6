ff74752fd11ccd6ad7d7e80fbd1c77962591f23f
leQwang <s3912105@rmit.edu.vn>
2023-11-19 12:22:22 +0700

    fix scroll frame 7

--- src/layouts/Frame7.tsx
+++ src/layouts/Frame7.tsx
@@ -19,18 +19,15 @@ const Frame7 = (props: Props) => {
         alt="frame 7 mobile"
       />
       <div className="relative w-full h-full flex flex-col items-center z-20">
-        <div className="w-[70%]">
-          <div className="flex flex-row space-x-20 items-center cursor-pointer">
-            <a href="https://www.riotgames.com/en">
-              <div className="relative w-32 h-28 xl:h-32 2xl:h-40 2xl:w-40  filter invert "></div>
-            </a>
-            <a href="https://vnggames.com/">
-              <div className="relative w-16 h-16 2xl:h-24 2xl:w-24 cursor-pointer"></div>
-            </a>
+        <div className="w-full md:w-[70%]">
+          <div className="flex flex-row space-x-20 items-center justify-center cursor-pointer text-white">
+            <div className="w-12 h-12 border text-center align-middle">RIOT GAME</div>
+            <div className="w-12 h-12 border text-center align-middle">VNG LOGO</div>
+            
           </div>
-          <div className="text-center text-sm px-4 md:text-lg 2xl:text-2xl space-y-6 text-white">
+          <div className="text-center text-sm px-4 md:text-lg 2xl:text-2xl space-y-2 md:space-y-6 text-white">
             <p>©️ 2022 Riot games, Inc</p>
-            <p className="w-[60%] mx-auto">
+            <p className="w-full md:w-[60%] mx-auto">
               Công ty Cổ Phần VNG. Địa chỉ: Lô 03b-04-05-06-07, đường số 13, khu
               Công Nghiệp, Khu Chế Xuất Tân Thuận, phường Tân Thuận Đông, Quận
               7, TP.HCM.
